7f0567319fd3f48a4c77debdd818dde550657261
Christopher Hill <thorsdaddy77@gmail.com>
2016-10-08 21:02:36 -0500

    modified renderer to fix bug

--- renderer.js
+++ renderer.js
@@ -1,29 +1,17 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-const button = document.createElement('button')
-button.textContent = 'hello world'
-button.className = 'btn'
-document.body.appendChild(button)
+'use strict'
+
+const token  = require('./key')
 
 
+const button = document.querySelector('#button')
+const input = document.querySelector('#input')
 
-const token =  '1a959ab612494d869ddc155a790b44bb'
-const url = 'http://www.reddit.com'
-
- 
-
-
-
-
-function reqListener () {
-  console.log(JSON.stringify(this.responseText));
-}
-
-const apiCall = new XMLHttpRequest();
-apiCall.addEventListener("load", reqListener);
-apiCall.open("GET", `https://api.dandelion.eu/datatxt/nex/v1/?url=${url}&include=types%2Cabstract%2Ccategories&token=${token}`);
-apiCall.send();
-
-
-
+button.addEventListener('click', function() {
+  let url = input.value
+  let test = new XMLHttpRequest()
+  test.open('GET', `https://api.dandelion.eu/datatxt/nex/v1/?url=${url}&include=types%2Cabstract%2Ccategories&token=${token}`)
+  test.send()
+  test.addEventListener('load', function() {
+    console.log(this.responseText)
+  })
+})
