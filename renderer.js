626de68b8e643dee5cfc915be6243ccc6b76da75
Christopher Hill <thorsdaddy77@gmail.com>
2016-10-08 20:03:07 -0500

    accessing api and console logging

--- renderer.js
+++ renderer.js
@@ -5,3 +5,25 @@ const button = document.createElement('button')
 button.textContent = 'hello world'
 button.className = 'btn'
 document.body.appendChild(button)
+
+
+
+const token =  '1a959ab612494d869ddc155a790b44bb'
+const url = 'http://www.reddit.com'
+
+ 
+
+
+
+
+function reqListener () {
+  console.log(JSON.stringify(this.responseText));
+}
+
+const apiCall = new XMLHttpRequest();
+apiCall.addEventListener("load", reqListener);
+apiCall.open("GET", `https://api.dandelion.eu/datatxt/nex/v1/?url=${url}&include=types%2Cabstract%2Ccategories&token=${token}`);
+apiCall.send();
+
+
+
